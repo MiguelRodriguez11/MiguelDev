03d912f0c8501c7ba689f94235f8d99852ed2a26
Miguel Rodriguez <11.MRodriguez.21@gmail.com>
2024-07-23 14:55:04 -0500

    [Add] constants

---
+++ linkBio/linkBio/constants.py
@@ -0,0 +1,7 @@
+#Cabecera
+GITHUB_URL = "https://github.com/MiguelRodriguez11"
+LINKEDIN_URL = "www.linkedin.com/in/miguel-angel-rodriguez-saray-218592139"
+
+#Otros
+SPOTIFY_URL = "https://open.spotify.com/user/12136310349?si=0c550444d6f84353"
+YOUTUBE_URL = "https://www.youtube.com/"
--- linkBio/linkBio/views/links/links.py
+++ linkBio/linkBio/views/links/links.py
@@ -1,4 +1,5 @@
 import reflex as rx
+import linkBio.constants as const
 from linkBio.components.link_button import link_button
 from linkBio.components.title import title
 from linkBio.styles.styles import Size
@@ -9,16 +10,22 @@ def links() -> rx.Component:
         link_button(
             "LinkedIn",
             "Mi perfil laboral",
-            "https://github.com/MiguelRodriguez11"
+            const.LINKEDIN_URL
         ),
         link_button(
             "GitHub",
             "Mi perfil de GitHub",
-            "https://github.com/MiguelRodriguez11"
+            const.GITHUB_URL
         ),
         link_button(
-            "YouTube", "Coming soon",
-             "https://youtube.com"
+            "Spotify",
+            "Playlist para codear",
+            const.SPOTIFY_URL
+        ),
+        link_button(
+            "YouTube",
+            "Coming soon",
+            const.YOUTUBE_URL
         ),
         width="100%"
     )
