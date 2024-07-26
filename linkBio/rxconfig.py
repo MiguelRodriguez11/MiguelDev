02d1a66053457208a959d14259a5101393169279
Miguel Rodriguez <11.MRodriguez.21@gmail.com>
2024-07-26 03:42:42 -0500

    [Update]

--- linkBio/rxconfig.py
+++ linkBio/rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="linkBio",
     cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://migueldev-web.vercel.app/"
+        "https://migueldev-web.vercel.app",
+        "http://localhost:3000"
     ]
 )
