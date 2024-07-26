f69133ec1cece490b3d8198d8582a2de4440a332
Miguel Rodriguez <11.MRodriguez.21@gmail.com>
2024-07-26 12:48:42 -0500

    [Udpdate] pylint

--- linkBio/linkBio/routes.py
+++ linkBio/linkBio/routes.py
@@ -1,6 +1,17 @@
+"""
+Este modulo configura las rutas.
+"""
+
 from enum import Enum
 
 
 class Route(Enum):
+    """
+    Enum para definir las rutas de la aplicación.
+
+    Attributes:
+        INDEX (str): Ruta para la página principal.
+        COURSES (str): Ruta para la página de cursos.
+    """
     INDEX = "/"
     COURSES = "/cursos"
